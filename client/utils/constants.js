e9dc60214f604514349131bfb99441356f7260d1
parthgala1 <parth.gala1356@gmail.com>
2024-11-24 22:31:10 +0530

    logout and useById updates

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -3,6 +3,7 @@ export const HOST = process.env.NEXT_PUBLIC_SERVER_URL;
 // Auth
 export const AUTH_ROUTE = `${HOST}/auth`;
 export const LOGIN_USER_ROUTE = `${AUTH_ROUTE}/login-user`;
+export const LOGOUT_ROUTE = `${AUTH_ROUTE}/logout-user`;
 
 // Sidebar
 export const SIDEBAR_DATA_ROUTE = `${HOST}/sidebar/get-sidebar-data`;
@@ -10,8 +11,14 @@ export const SIDEBAR_DATA_ROUTE = `${HOST}/sidebar/get-sidebar-data`;
 // Patients
 export const PATIENT_ROUTE = `${HOST}/patient`;
 export const ADD_PATIENT_ROUTE = `${PATIENT_ROUTE}/add-patient`;
+export const GET_PAT_BY_ID_ROUTE = `${PATIENT_ROUTE}/get-patient-by-id`;
 
 // Student Therapists
 export const STUDENT_THERAPIST_ROUTE = `${HOST}/student-therapist`;
 export const ADD_STUDENT_THERAPIST_ROUTE = `${STUDENT_THERAPIST_ROUTE}/add-student`;
-export const GET_USER_BY_ID_ROUTE = `${STUDENT_THERAPIST_ROUTE}/get-student-by-id`;
+export const GET_STT_BY_ID_ROUTE = `${STUDENT_THERAPIST_ROUTE}/get-student-by-id`;
+
+// Supervisors
+export const SUPERVISOR_ROUTE = `${HOST}/supervisor`;
+export const ADD_SUPERVISOR_ROUTE = `${SUPERVISOR_ROUTE}/add-supervisor`;
+export const GET_SUP_BY_ID_ROUTE = `${SUPERVISOR_ROUTE}/get-supervisor-by-id`;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/login-user", loginUser);
+router.post("/logout-user", logoutUser);
 // router.post("/register-user", registerUser);
 
 export default router;
