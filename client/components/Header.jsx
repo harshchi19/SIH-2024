2f8558d7581f8aa77bcaff49419d2a5b9e6eb3d6
sameer240704 <sameergupta79711@gmail.com>
2024-11-24 22:05:55 +0530

    Middle commit to resolve merge conflict

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
 
     const breadcrumbList = pathParts.map((part, index) => {
       const normalizedPart = part.trim().toLowerCase();
-      let label = dict?.breadcrumb?.[normalizedPart];
+      let label = dict?.breadcrumb?.[normalizedPart] || normalizedPart;
 
       const isRole = ["student-therapist", "supervisor", "patient"].includes(
         normalizedPart
